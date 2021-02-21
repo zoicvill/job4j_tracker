727b6fd74e6fba0c6c8a6563318ff3a174cec1fd
zoicvil <891038713645Qaz>
2021-02-21 13:54:31 +0300

    Состояние объекта [#465631]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int per){
+        this.load = per;
+    }
+
+    public void exchange(Battery another){
+        another.load += this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery height = new Battery(70);
+        Battery low = new Battery(20);
+        System.out.println(height.load + " " + low.load);
+        height.exchange(low);
+        System.out.println(height.load + " " + low.load);
+    }
+}
