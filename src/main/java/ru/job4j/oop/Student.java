44f46c37783e4a47d3417083a5a58fa957cadd3f
zoicvil <891038713645Qaz>
2021-02-21 12:31:23 +0300

    Вызов метода объекта [#465627]

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+public class Student {
+    public static void main(String[] args) {
+        Student studentSongAMus = new Student();
+        studentSongAMus.song();
+        studentSongAMus.music();
+        studentSongAMus.song();
+        studentSongAMus.song();
+        studentSongAMus.music();
+        studentSongAMus.music();
+    }
+
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+    public void music(){
+        System.out.println("Play music");
+    }
+}
