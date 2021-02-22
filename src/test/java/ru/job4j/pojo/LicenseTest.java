7a0179f19d5b933eb1e1ed544b073cafc287c90c
zoicvil <891038713645Qaz>
2021-02-22 19:10:28 +0300

    Сравнение моделей. Метод equals [#465647]

---
+++ src/test/java/ru/job4j/pojo/LicenseTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.pojo;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class LicenseTest {
+
+    @Test
+    public void eqName() {
+        License first = new License();
+        first.setCode("audio");
+        License second = new License();
+        second.setCode("audio");
+        assertThat(first, is(second));
+    }
+}
