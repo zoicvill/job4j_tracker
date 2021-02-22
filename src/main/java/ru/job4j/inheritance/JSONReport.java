551b919118403be28b46f68983991505d22057f7
zoicvil <891038713645Qaz>
2021-02-22 13:19:11 +0300

    Переопределение [#465638]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,12 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    @Override
+    public String generate(String name, String text) {
+//      return "{ \n \"name\" : " + "\"" + name +"\"" +"," + "\n \"text\" : " + "\"" + text +"\"" + "\n}";
+        return "{ " + System.lineSeparator()
+                + " \"" + name + "\"" + " : " + "\"" + "name" + "\"" + "," + System.lineSeparator()
+                + " \"" + text + "\"" + " : " + "\"" + "text" + "\"" + System.lineSeparator()
+                + "}";
+    }
+}
---
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -0,0 +1,12 @@
+package ru.job4j.inheritance;
+
+public class ReportUsage {
+    public static void main(String[] args) {
+        TextReport report = new TextReport();
+        String s = report.generate("Go generate name", "Generate text");
+        System.out.println(s+"\n");
+        JSONReport jsonReport = new JSONReport();
+        String ss = jsonReport.generate("JSONReport generate name", "JSONGenerate text");
+        System.out.println(ss);
+    }
+}
