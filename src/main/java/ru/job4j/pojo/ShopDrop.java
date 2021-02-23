c0f7daabdbb42a6aa7aa5855cf93a6d555ae963f
zoicvil <891038713645Qaz>
2021-02-23 12:49:00 +0300

    Удаление моделей из массива. [#465652]

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -2,14 +2,10 @@ package ru.job4j.pojo;
 
 public class ShopDrop {
     public static Product[] leftShift(Product[] products, int index) {
-        for (int i = index; i < products.length; i++) {
-            if (i == products.length - 1) {
-                products[i] = null;
-            } else {
-                products[i] = products[i + 1];
-            }
-
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
         }
+        products[products.length - 1] = null;
         return products;
     }
 }
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,7 +1,5 @@
 package ru.job4j.tracker;
 
-import ru.job4j.tracker.Item;
-
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
