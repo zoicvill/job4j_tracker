50e2dbcb4c7ac2b17b339471c2198dbb629802e9
zoicvil <891038713645Qaz>
2021-02-25 15:14:24 +0300

    Интерфейс Input [#465576]

---
+++ src/main/java/ru/job4j/tracker/Bus.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker;
+
+public class Bus implements Transport{
+    @Override
+    public void goTrans() {
+        System.out.println("Автобус едет");
+    }
+
+    @Override
+    public void passTrans(int qanPass) {
+        System.out.println("Пассажиров в автобусе " + qanPass);
+    }
+
+    @Override
+    public int tankUp(int gasoline) {
+        System.out.println("Стоимость пути " + gasoline * 50 + " руб.");
+        return gasoline * 50;
+    }
+
+    public static void main(String[] args) {
+        Bus bus = new Bus();
+        bus.goTrans();
+        bus.tankUp(50);
+        bus.passTrans(70);
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker;
+
+public interface Transport {
+    void goTrans();
+    void passTrans(int qanPass);
+    int tankUp(int gasoline);
+
+
+}
