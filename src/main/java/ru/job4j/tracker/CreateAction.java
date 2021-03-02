6b3c92d2556b6661d6cbb4faa21a26cc011aef2f
zoicvil <891038713645Qaz>
2021-03-02 11:38:24 +0300

    Реализация меню за счет шаблона стратегия. [#465582]

--- src/main/java/ru/job4j/tracker/CreateAction.java
+++ src/main/java/ru/job4j/tracker/CreateAction.java
@@ -17,13 +17,7 @@ public class CreateAction implements UserAction{
         String name = input.askStr("Enter name: ");
         Item item = new Item(name);
         tracker.add(item);
-        genItem(tracker);
         return true;
     }
-    private void genItem(Tracker tracker) {
-        for (int i = 0; i < 10; i++) {
-            Item item = new Item("GenItem" + i * 3);
-            tracker.add(item);
-        }
-    }
+
 }
