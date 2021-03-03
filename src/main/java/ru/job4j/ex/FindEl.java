d1b2d7e4c3b85f2c3ff85a7466f401b80bec5b55
zoicvil <891038713645Qaz>
2021-03-03 14:50:37 +0300

    0.4. Пользовательские исключения. [#465596]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,23 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key))
+                return i;
+        }
+        if (rsl == -1) throw new ElementNotFoundException("Элемент не найден");
+
+        return rsl;
+    }
+
+    public static void main(String[] args) throws  ElementNotFoundException{
+        try {
+            FindEl.indexOf(new String[]{"fg","ss","gg", "hh"},"ss4");
+        }
+        catch (ElementNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
