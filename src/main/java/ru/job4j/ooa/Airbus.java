0ef0d769c22031b2bcf9994e216c2219398d6a24
zoicvil <891038713645Qaz>
2021-03-05 10:44:25 +0300

    final [#465614]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -1,12 +1,8 @@
 package ru.job4j.ooa;
 
-public final class Airbus extends Aircraft{
+public final class Airbus extends Aircraft {
     private final static int COUNT_ENGINE = 2;
     private String name;
-
-    public Airbus(String name) {
-        this.name = name;
-    }
 
     public String getName() {
         return name;
@@ -22,9 +18,9 @@ public final class Airbus extends Aircraft{
     }
 
     public void printCountEngine(String name) {
-        if (name.equals("A380")){
-        System.out.println("Количество двигателей равно: " + 4);}
-        else {
+        if (name.equals("A380")) {
+            System.out.println("Количество двигателей равно: " + 4);
+        } else {
             System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
         }
 
--- src/main/java/ru/job4j/ooa/Airport.java
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -2,16 +2,17 @@ package ru.job4j.ooa;
 
 public class Airport {
     public static void main(String[] args) {
-        Airbus airbus = new Airbus("A320");
+        Airbus airbus = new Airbus();
+        airbus.setName("A320");
         System.out.println(airbus);
         airbus.printModel();
         airbus.printCountEngine(airbus.getName());
-        System.out.println();
+        System.out.println("---------");
 
-        airbus = new Airbus("A380");
-        System.out.println(airbus);
-        airbus.printCountEngine(airbus.getName());
+        airbus = new Airbus();
         airbus.setName("A380");
         System.out.println(airbus);
+        airbus.printModel();
+        airbus.printCountEngine(airbus.getName());
     }
 }
