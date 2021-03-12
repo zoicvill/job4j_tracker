8c0f22815e78b00b3423361878975862f667525d
zoicvil <891038713645Qaz>
2021-03-12 10:08:41 +0300

    0. Map, HashMap [#465561]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("3433@ya.tu","Rori Lom");
+        map.put("1111@ya.tu","Tommi Boobs");
+        map.put("1111@ya.tu","Tommi Bo");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+}
