fe1005817b852c94272a02bcf2686badadcec4ad
zoicvil <891038713645Qaz>
2021-03-14 18:54:54 +0300

    3. Компаратор для строк [#465547] up убрал лишний счетчик

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -7,13 +7,11 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int rsl = Integer.compare(left.length(), right.length());
-        int j = 0;
         for (int i = 0; i < (Math.min(left.length(), right.length())); i++) {
-            if (left.charAt(i) != right.charAt(j)) {
-                rsl = Character.compare(left.charAt(i), right.charAt(j));
+            if (left.charAt(i) != right.charAt(i)) {
+                rsl = Character.compare(left.charAt(i), right.charAt(i));
                 break;
             }
-            j++;
         }
         return rsl;
     }
