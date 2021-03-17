cccb33072bf4d14454ece0a9acf6e2a4e92a34f6
zoicvil <891038713645Qaz>
2021-03-17 20:59:31 +0300

    4. Зона видимости в лямбда-выражениях и исключения [#465375]

---
+++ src/main/java/ru/job4j/function/ScopeInside.java
@@ -0,0 +1,22 @@
+package ru.job4j.function;
+
+import java.util.function.Supplier;
+
+public class ScopeInside {
+
+    public static void main(String[] args) {
+        int[] number = {1, 2, 3};
+        int total = 0;
+        for (int i = 0; i < number.length; i++) {
+            int num = i;
+            total += add(
+                    () -> number[num]
+            );
+        }
+        System.out.println(total);
+    }
+
+    private static Integer add(Supplier<Integer> calc) {
+        return calc.get();
+    }
+}
