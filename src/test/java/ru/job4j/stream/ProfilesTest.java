da2f9c7576128979b55d576b062a196433939e82
zoicvil <891038713645Qaz>
2021-03-18 20:52:05 +0300

    2. Список адресов [#465387] Заменил Stream.of в тесте на List.of

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -3,8 +3,6 @@ package ru.job4j.stream;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -12,21 +10,19 @@ import static org.junit.Assert.assertThat;
 public class ProfilesTest {
     @Test
     public void whenTest() {
-        List<Address> addr = Stream.of(
+        List<Address> addr = List.of(
                 new Address("Fore", "Goolo", 12, 15),
-                new Address("Uore", "Polo", 16, 75))
-                .collect(Collectors.toList());
+                new Address("Uore", "Polo", 16, 75));
 
-        List<Profile> profiles = Stream.of(
+        List<Profile> profiles = List.of(
                 new Profile(addr.get(0)),
                 new Profile(addr.get(1))
-        ).collect(Collectors.toList());
+        );
 
-        List<Address> res = Stream.of(
+        List<Address> res = List.of(
                 new Address("Fore", "Goolo", 12, 15),
-                new Address("Uore", "Polo", 16, 75))
-                .collect(Collectors.toList());
-        
+                new Address("Uore", "Polo", 16, 75));
+
         assertThat(Profiles.collectProf(profiles), is(res));
     }
 
