355e2f4e90b11e8166dad6b0189e68ce83bc8dca
zoicvil <891038713645Qaz>
2021-03-18 12:42:26 +0300

    6. Функции высшего порядка [#465372]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,22 +1,26 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
-    ArrayList<Person> personArr = new ArrayList<>();
+    private ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person p) {
-        this.personArr.add(p);
+        this.persons.add(p);
     }
 
-    public ArrayList<Person> search(String key) throws IndexOutOfBoundsException {
-        ArrayList<Person> result = new ArrayList<>();
+    public ArrayList<Person> search(String key) {
+        Predicate<Person> name = pes -> pes.getName().contains(key);
+        Predicate<Person> lastName = pes -> pes.getName().contains(key);
+        Predicate<Person> phone = pes -> pes.getName().contains(key);
+        Predicate<Person> address = pes -> pes.getName().contains(key);
+        Predicate<Person> combine = pes -> name.test(pes) || lastName.test(pes)
+                || phone.test(pes) || address.test(pes);
 
-        for (Person person : personArr) {
-            if (person.getAddress().contains(key) ||
-                    person.getName().contains(key) ||
-                    person.getLastName().contains(key) ||
-                    person.getPhone().contains(key)) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
