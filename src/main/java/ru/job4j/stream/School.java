b705426126686c63f22c4dca979034ba6a3c75c6
zoicvil <891038713645Qaz>
2021-03-19 09:52:11 +0300

    4. Преобразование List в Map. [#465389]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,13 +1,18 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
-    public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
+    public Map<String, Student> collect(List<Student> students, Predicate<Student> predicate) {
         return students.stream()
                 .filter(predicate)
-                .collect(Collectors.toList());
+                .distinct()
+                .collect(Collectors.toMap(
+                        Student::getSurname,
+                        s -> s
+                ));
     }
 }
--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -2,8 +2,9 @@ package ru.job4j.stream;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 import static org.hamcrest.core.Is.is;
@@ -16,15 +17,18 @@ public class SchoolTest {
                 new Student(10, "Surname1"),
                 new Student(40, "Surname4"),
                 new Student(50, "Surname5"),
+                new Student(50, "Surname5"),
                 new Student(70, "Surname7"),
+                new Student(70, "Surname7"),
+                new Student(90, "Surname9"),
                 new Student(90, "Surname9")
         );
         School sc = new School();
         Predicate<Student> pr = student -> student.getScore() >= 70;
-        List<Student> rsl = sc.collect(students, pr);
-        List<Student> expected = new ArrayList<>();
-        expected.add(new Student(70, "Surname7"));
-        expected.add(new Student(90, "Surname9"));
+        Map<String, Student> rsl = sc.collect(students, pr);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Surname7", new Student(70, "Surname7"));
+        expected.put("Surname9", new Student(90, "Surname9"));
         assertThat(rsl, is(expected));
     }
 
@@ -33,17 +37,25 @@ public class SchoolTest {
         List<Student> students = List.of(
                 new Student(20, "Surname2"),
                 new Student(30, "Surname3"),
+                new Student(30, "Surname3"),
+                new Student(30, "Surname3"),
+                new Student(50, "Surname5"),
+                new Student(50, "Surname5"),
                 new Student(50, "Surname5"),
                 new Student(60, "Surname6"),
+                new Student(60, "Surname6"),
+                new Student(60, "Surname6"),
+                new Student(80, "Surname8"),
+                new Student(80, "Surname8"),
                 new Student(80, "Surname8")
         );
         School sc = new School();
         Predicate<Student> pr = student -> student.getScore() >= 50
                 && student.getScore() < 70;
-        List<Student> rsl = sc.collect(students, pr);
-        List<Student> expected = new ArrayList<>();
-        expected.add(new Student(50, "Surname5"));
-        expected.add(new Student(60, "Surname6"));
+        Map<String, Student> rsl = sc.collect(students, pr);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Surname5", new Student(50, "Surname5"));
+        expected.put("Surname6", new Student(60, "Surname6"));
         assertThat(rsl, is(expected));
     }
 
@@ -51,18 +63,27 @@ public class SchoolTest {
     public void whenCollectClassV() {
         List<Student> students = List.of(
                 new Student(10, "Surname1"),
+                new Student(10, "Surname1"),
+                new Student(10, "Surname1"),
+                new Student(30, "Surname3"),
+                new Student(30, "Surname3"),
                 new Student(30, "Surname3"),
                 new Student(40, "Surname4"),
+                new Student(40, "Surname4"),
+                new Student(40, "Surname4"),
                 new Student(60, "Surname6"),
+                new Student(90, "Surname9"),
+                new Student(90, "Surname9"),
+                new Student(90, "Surname9"),
                 new Student(90, "Surname9")
         );
         School sc = new School();
         Predicate<Student> pr = student -> student.getScore() < 50;
-        List<Student> rsl = sc.collect(students, pr);
-        List<Student> expected = new ArrayList<>();
-        expected.add(new Student(10, "Surname1"));
-        expected.add(new Student(30, "Surname3"));
-        expected.add(new Student(40, "Surname4"));
+        Map<String, Student> rsl = sc.collect(students, pr);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Surname1", new Student(10, "Surname1"));
+        expected.put("Surname3", new Student(30, "Surname3"));
+        expected.put("Surname4", new Student(40, "Surname4"));
         assertThat(rsl, is(expected));
     }
 
